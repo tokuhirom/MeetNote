ff407f1ecdf03d4ab7683a947432a1d133161a0b
Tokuhiro Matsuno <tokuhirom@gmail.com>
2023-11-22 23:33:52 +0900

    automatic log refresh

--- composeApp/src/desktopMain/kotlin/App.kt
+++ composeApp/src/desktopMain/kotlin/App.kt
@@ -10,6 +10,7 @@ import androidx.compose.material.Divider
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -17,6 +18,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.delay
 import org.jetbrains.compose.resources.ExperimentalResourceApi
 import org.jetbrains.compose.resources.painterResource
 import org.slf4j.LoggerFactory
@@ -33,11 +35,13 @@ fun App(dataRepository: DataRepository) {
     MaterialTheme {
         var logs by remember { mutableStateOf(dataRepository.getRecentSummarizedFiles()) }
 
-        val executor = Executors.newScheduledThreadPool(1)
-        executor.scheduleAtFixedRate({
-            logger.info("Loading recent summarized files")
-            logs = dataRepository.getRecentSummarizedFiles()
-         }, 0, 10, TimeUnit.SECONDS)
+        LaunchedEffect(Unit) {
+            while(true) {
+                logger.info("Refreshing logs")
+                logs = dataRepository.getRecentSummarizedFiles()
+                delay(10000L)
+            }
+        }
 
         Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
             LazyColumn(modifier = Modifier.weight(1f)) {
