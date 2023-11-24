ad4b0008683b7929e12bea4c1bc5cad7359f07d1
Tokuhiro Matsuno <tokuhirom@gmail.com>
2023-11-24 23:40:01 +0900

    cleanup COnfigurationDialog

--- composeApp/src/desktopMain/kotlin/ConfigurationDialog.kt
+++ composeApp/src/desktopMain/kotlin/ConfigurationDialog.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Button
@@ -17,7 +18,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.unit.DpSize
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.DialogWindow
 import androidx.compose.ui.window.rememberDialogState
@@ -28,114 +28,108 @@ import java.time.Duration
 fun configurationDialog(configRepository: ConfigRepository, onClose: () -> Unit) {
     val logger = LoggerFactory.getLogger("configurationDialog")
 
-    DialogWindow(onClose,
+    DialogWindow(
+        onCloseRequest = onClose,
         title = "Configuration",
         resizable = true,
-        state = rememberDialogState(size = DpSize(600.dp, 400.dp))) {
+        state = rememberDialogState(width = 800.dp, height = 600.dp)
+    ) {
         Column(modifier = Modifier.padding(8.dp)) {
             var config by remember { mutableStateOf(configRepository.loadSettings()) }
 
-            Box {
+            Box(modifier = Modifier.padding(bottom = 10.dp)) {
                 Column {
                     Text("OpenAI Configuration")
-
                     TextField(
                         value = config.apiToken ?: "",
-                        onValueChange = {
-                            config.apiToken = it
-                        },
-                        label = {
-                            Text("API Token")
-                        }
+                        onValueChange = { config = config.copy(apiToken = it) },
+                        label = { Text("API Token") }
                     )
                 }
             }
 
-            Box {
+            Box(modifier = Modifier.padding(bottom = 10.dp)) {
                 Column {
-                    Row {
-                        Text("Recorder Controller Configurations")
-                        Spacer(modifier = Modifier.weight(1f))
-                        Button(
-                            onClick = {
-                                config = config.copy(
-                                    recorderControllerConfig = config.recorderControllerConfig.copy(
-                                        windowNamePatterns = config.recorderControllerConfig.windowNamePatterns + WindowNamePattern("com.example", "Great new window")
-                                    )
+                    Text("Recorder Controller Configurations")
+                    Button(
+                        onClick = {
+                            config = config.copy(
+                                recorderControllerConfig = config.recorderControllerConfig.copy(
+                                    windowNamePatterns = config.recorderControllerConfig.windowNamePatterns +
+                                            WindowNamePattern("com.example", "Great new window")
                                 )
-                            }
-                        ) {
-                            Text("Add")
-                        }
+                            )
+                        },
+                    ) {
+                        Text("Add")
                     }
 
-                    LazyColumn {
-                        items(config.recorderControllerConfig.windowNamePatterns) { windowNamePattern ->
-                            Row {
-                                TextField(
-                                    value = windowNamePattern.bundleId,
-                                    onValueChange = {
-                                        config = config.copy(
-                                            recorderControllerConfig = config.recorderControllerConfig.copy(
-                                                windowNamePatterns = config.recorderControllerConfig.windowNamePatterns.map {
-                                                    if (it == windowNamePattern) {
-                                                        windowNamePattern.copy(bundleId = it.bundleId)
-                                                    } else {
-                                                        it
+                    Box(modifier = Modifier.padding(top = 10.dp)) {
+                        LazyColumn {
+                            items(config.recorderControllerConfig.windowNamePatterns) { windowNamePattern ->
+                                Row {
+                                    TextField(
+                                        value = windowNamePattern.bundleId,
+                                        onValueChange = {value ->
+                                            config = config.copy(
+                                                recorderControllerConfig = config.recorderControllerConfig.copy(
+                                                    windowNamePatterns = config.recorderControllerConfig.windowNamePatterns.map {
+                                                        if (it == windowNamePattern) {
+                                                            it.copy(bundleId = value)
+                                                        } else {
+                                                            it
+                                                        }
                                                     }
-                                                }
+                                                )
                                             )
-                                        )
-                                    },
-                                    label = {
-                                        Text("Bundle ID")
-                                    },
-                                    modifier = Modifier.weight(1f)
-                                )
+                                        },
+                                        label = { Text("Bundle ID") },
+                                        modifier = Modifier.weight(1f, fill = false)
+                                    )
 
-                                TextField(
-                                    value = windowNamePattern.windowName,
-                                    onValueChange = {
-                                        config = config.copy(
-                                            recorderControllerConfig = config.recorderControllerConfig.copy(
-                                                windowNamePatterns = config.recorderControllerConfig.windowNamePatterns.map {
-                                                    if (it == windowNamePattern) {
-                                                        windowNamePattern.copy(windowName = it.windowName)
-                                                    } else {
-                                                        it
+                                    Spacer(modifier = Modifier.width(10.dp))
+
+                                    TextField(
+                                        value = windowNamePattern.windowName,
+                                        onValueChange = {value ->
+                                            config = config.copy(
+                                                recorderControllerConfig = config.recorderControllerConfig.copy(
+                                                    windowNamePatterns = config.recorderControllerConfig.windowNamePatterns.map {
+                                                        if (it == windowNamePattern) {
+                                                            it.copy(windowName = value)
+                                                        } else {
+                                                            it
+                                                        }
                                                     }
-                                                }
+                                                )
                                             )
-                                        )
-                                    },
-                                    label = {
-                                        Text("Window Name")
-                                    },
-                                    modifier = Modifier.weight(1f)
-                                )
+                                        },
+                                        label = { Text("Window Name") },
+                                        modifier = Modifier.weight(1f, fill = false)
+                                    )
 
-                                Button(
-                                    onClick = {
-                                        config = config.copy(
-                                            recorderControllerConfig = config.recorderControllerConfig.copy(
-                                                windowNamePatterns = config.recorderControllerConfig.windowNamePatterns.filter {
-                                                    it != windowNamePattern
-                                                }
+                                    Spacer(modifier = Modifier.width(10.dp))
+
+                                    Button(
+                                        onClick = {
+                                            config = config.copy(
+                                                recorderControllerConfig = config.recorderControllerConfig.copy(
+                                                    windowNamePatterns = config.recorderControllerConfig.windowNamePatterns.filterNot {
+                                                        it == windowNamePattern
+                                                    }
+                                                )
                                             )
-                                        )
-                                    },
-                                    colors = ButtonDefaults.buttonColors(
-                                        backgroundColor = Color.Red
-                                    ),
-                    //                            modifier = Modifier.weight(1f)
-                                ) {
-                                    Text("Delete")
+                                        },
+                                        colors = ButtonDefaults.buttonColors(backgroundColor = Color.Red)
+                                    ) {
+                                        Text("Delete")
+                                    }
                                 }
                             }
                         }
                     }
 
-                    var sleepIntervalIsError  by remember { mutableStateOf(false) }
+                    var sleepIntervalIsError by remember { mutableStateOf(false) }
 
                     TextField(
                         value = config.recorderControllerConfig.sleepInterval.toSeconds().toString(),
@@ -154,12 +148,11 @@ fun configurationDialog(configRepository: ConfigRepository, onClose: () -> Unit)
                             }
                         },
                         isError = sleepIntervalIsError,
-                        label = {
-                            Text("Sleep Interval [sec]")
-                        }
+                        label = { Text("Sleep Interval [sec])") }
                     )
 
-                    var maxRecordingDurationIsError  by remember { mutableStateOf(false) }
+                    var maxRecordingDurationIsError by remember { mutableStateOf(false) }
+
                     TextField(
                         value = config.recorderControllerConfig.maxRecordingDuration.toMinutes().toString(),
                         onValueChange = { value ->
@@ -178,20 +171,24 @@ fun configurationDialog(configRepository: ConfigRepository, onClose: () -> Unit)
                         },
                         isError = maxRecordingDurationIsError,
                         label = {
-                            Text("Max recording duration [min]\n(MP3 file size limit is 25MB..\nIf OpenAPI returns an error, you need to decrease this value.)")
+                            Text(
+                                "Max recording duration [min]\n(MP3 file size limit is 25MB..\nIf OpenAPI returns " +
+                                        "an error, you need to decrease this value.)"
+                            )
                         }
                     )
-                }
-            }
 
-
-            Button(
-                onClick = {
-                    configRepository.saveConfiguration(config)
-                    onClose()
+                    Box(modifier = Modifier.padding(top = 10.dp)) {
+                        Button(
+                            onClick = {
+                                configRepository.saveConfiguration(config)
+                                onClose()
+                            }
+                        ) {
+                            Text("Save")
+                        }
+                    }
                 }
-            ) {
-                Text("Save")
             }
         }
     }
